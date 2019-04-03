abca2bb80baeed9b678135fb4d278e1c9240f4fb
carlosaqf <carlosfegurgur@gmail.com>
2019-04-03 10:11:00 -0500

    created blog title. removed link to homepage

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import styled from 'styled-components'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const BlogWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fafafa;
+  padding-bottom: 10px;
+  margin: 0 auto;
+  h2{
+      font-size: 2em;
+      letter-spacing: 0.8em;
+      font-weight: 200;
+      padding-top: 1em;
+      text-align: center;
+      text-transform: uppercase;
+  }
+`;
+
 
 const Blog = () => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Blog Us</h1>
-    <p>Welcome to the Blog Page. Here I will explain who I am and what
-    it is I hope to accomplish with this blog. Get to know me!
-    </p>
-    <Link to="/">Go back to the homepage</Link>
+      <BlogWrapper>
+        <h2>Blog</h2>
+        <p>Welcome to the Blog Page. Here I will explain who I am and what
+          it is I hope to accomplish with this blog. Get to know me!
+        </p>
+      </BlogWrapper>
   </Layout>
 )
 
