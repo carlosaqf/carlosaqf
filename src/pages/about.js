fc0769bd98cad719d8330e3490744e9557703dbb
carlosaqf <carlosfegurgur@gmail.com>
2019-04-03 10:10:44 -0500

    added mobile responsiveness. input some about page information

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from 'styled-components'
+import { device } from '../components/devices'
 
 const AboutContainer = styled.div`
   margin: 0 auto;
@@ -12,15 +13,26 @@ const AboutTitle = styled.h1`
   text-align: center;
   padding-top: 1.5em;
   color: white;
+
+  @media ${device.mobileS}{
+    font-size: 0.7em;
+    font-weight: 300;
+    letter-spacing: 3px;
+    text-align: end;
+    padding-top: 11em;
+    padding-right: 0.5em;
+  }
 `;
 
 const AboutParagraph = styled.p`
-  text-indent: 2em;
   padding: 2em 1em 0 1em;
+  width: 70vw;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const AboutPicture = styled.div`
-  background: url("https://source.unsplash.com/random/1920x1080");
+  background: url("https://lh3.googleusercontent.com/_3DMiHGpuiWhiQmy2-zBnsHUx15bn-584wHHnyBTZnVWtIR9JTeiGEaWNYaExF1zJ4DdkJC4MsMgYAyRN8LaRnaHopLvepUoDga7EWcbRNF6BoAOGWBe_p3eF4X14j3ZQ-xgA6LxAZfBk0xgM4Er7v9WrTUYEiCcydJmxzuuAmpKHfpDfdzMbMbNjhIp3-EzyrXlUA8oi09j7rg_VrNsE6pUujVuAZ2nOemIV6hKc-l3GLCeqGxoEQDw60I3INv01hF8E7kdb9pycq_-JFaIfV3llKxZXpIhCUVjPBH3e-BbM5JgrVWX-jIvOJxP1y23wZQlDSmJzZkezOR6syGomlL2N0FBpOB4XWRy_si7wjD9CoO_T3h0Vhnlsum1WO7cPxMj6UR78GJyzkIGE3Q0uvmHFyxbewISwu5jFILf6jAOrXRS2AkpwxJgVZNGrcXx4Q0q4dN3Nq5Qz7Zqb_xbDMxMSKpEkaSjigEwrJjmV8cBJPl39yWgGDbX7g9ps0H3lYKmI8db2dd7K98XXWtYHR_pvBkWjt5p5LARWgxbSpN1Xmt5QX-qpLDSysR5V3hIP6vJB6hWf4JWN75Bo3CKM2nqVOzDXA5wLM5y=s2364-w2364-h1332-no");
   background-size: cover;
   background-position: center;
   height: 25vh;
@@ -28,9 +40,10 @@ const AboutPicture = styled.div`
 `;
 
 const AboutSubheading = styled.h3`
-  font-size: 1.5em;
-  padding-top: 1em;
-
+  @media ${device.mobileS}{
+    text-align: start;
+    padding-top: ${props => props.top || 0}em;
+  }
 `;
 
 const About = () => (
@@ -38,20 +51,21 @@ const About = () => (
     <SEO title="About" />
     <AboutContainer>
       <AboutPicture>
-        <AboutTitle>Hafa Adai! My name is Carlos Fegurgur!</AboutTitle>
+        <AboutTitle>Tumon, Guam</AboutTitle>
       </AboutPicture>
       <AboutParagraph>
 
-        <AboutSubheading>From Guam to Chicago</AboutSubheading>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, fugit!
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia atque illo ab sunt consequatur sapiente ad ratione tempora doloremque sint. Fugiat, deserunt. Minima laudantium mollitia quisquam, inventore cum eum itaque magni impedit ullam voluptates sequi architecto ratione molestiae? Quisquam nobis omnis illum blanditiis fugiat consequuntur expedita cumque eaque sint nulla.
+        <AboutSubheading>A Brief History</AboutSubheading>
+        In 2016, I graduated with a double major in Mathematics and Computer Science from the University of Guam. 
+        I then spent the next year living and playing rugby in England and travelling the globe. 
+        I returned home to Guam in late 2017, made the decision to move to the continental United States, and by Summer 2018, 
+        I had fully relocated to the city of Chicago.
         
-        <AboutSubheading>Looking Forward</AboutSubheading>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi perspiciatis autem praesentium, error quia voluptatem beatae. Soluta sint dicta tenetur deserunt, exercitationem unde commodi dolorem praesentium magni doloremque ab dolor delectus aspernatur voluptas nam in minus beatae veniam natus et quia rerum itaque quasi odio! Voluptatibus iusto ipsum ullam? Ipsam deleniti non reprehenderit voluptatem necessitatibus reiciendis corrupti! Delectus perferendis ullam minus perspiciatis, magni molestias nisi? Aperiam nulla dolore corporis fuga dolores libero laborum non harum.
+        <AboutSubheading top="1">Looking Forward</AboutSubheading>
+        
 
       </AboutParagraph>
     </AboutContainer>
-    <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
