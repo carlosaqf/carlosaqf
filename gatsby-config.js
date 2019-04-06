1cd4a0efc1527abf1856762beeb814a212588a50
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-04-05 19:43:39 -0500

    added gatsby-source-filesystem and gatsby-transformer-remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,13 @@ module.exports = {
     author: `@carlosaqf`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-react-helmet`, 
     `gatsby-plugin-styled-components`,
     {
@@ -52,6 +59,15 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-remark`,
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
