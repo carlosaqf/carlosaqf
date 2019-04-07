c09e47c1b76f41a33bdf7052f9f74a1c9e9bd174
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-04-07 17:59:44 -0500

    imported color from colors.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,10 @@
 import styled, { css } from 'styled-components'
+import { color } from './colors'
 
 export const Button = styled.button`
     background: transparent;
     border-radius: 3px;
-    border: 2px solid #c8a415;
+    border: 2px solid ${color.DARK};
     color: white;
     margin: 0 1em;
     padding: 0.25em 1em;
@@ -11,7 +12,7 @@ export const Button = styled.button`
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 
     ${props => props.primary && css`
-        background: #c8a415;
+        background: ${color.DARK};
         color: #white;
     `}
 `;
