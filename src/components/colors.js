11d80cb45773e40f5496e2f6d3a42e3d8d440f86
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-04-07 17:58:56 -0500

    exported colors out of their own js file

--- src/components/colors.js
+++ src/components/colors.js
@@ -8,6 +8,40 @@
 
     primary text #616161
     secondary text #616161
+
+
+    dark yellow = C8A415
+
+
+
     
+
+*/
+
+
+
+
+
+export const color = {
+    PRIMARY: '#c8a415',
+    SECONDARY: '#ffd54f',
+    
+    DARK: 'rgba(200,164,21,1)',
+    LIGHT: 'rgba(255, 213, 79, 1)',
+    GRAY: 'rgba(97, 97, 97, 1)',
+    WHITE: 'rgba(253, 255, 252, 1)',
+    BLUE: 'rgba(35, 87, 137, 1)',
+
+
+}
+
+
+/*
+yellow #F4D06F
+black #0D1F22
+grey blue #232C33
+blue #0075A2
+white #ECEBF3
+
 
 */
