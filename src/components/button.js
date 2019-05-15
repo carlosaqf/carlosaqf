4747651ccd0c8298de18d6d9528214e88965e342
carlosaqf <carlosfegurgur@gmail.com>
2019-05-15 07:12:04 -0500

    add box shadow to button class

--- src/components/button.js
+++ src/components/button.js
@@ -10,7 +10,7 @@ export const Button = styled.button`
     padding: 0.25em 1em;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-
+    box-shadow: 0 3px 4px 0 rgba(0,0,0,0.5);
     ${props => props.primary && css`
         background: ${color.DARK};
         color: #white;
