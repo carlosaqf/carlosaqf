53b716705ff5b5fbea7dbd12fad54a5f579d5001
carlosaqf <carlosfegurgur@gmail.com>
2019-06-16 15:09:57 -0500

    replace contact form with email link for simplicity/ease of use

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby'
 import Socials from './Socials'
 import { device } from './devices'
 import { Button } from '../components/button'
+import { color } from './colors'
 
 const ConnectContainer = styled.div`
     margin: 0 auto;
@@ -27,90 +28,19 @@ const ConnectContainer = styled.div`
         }
     }
 
-`;
-
-const FormContainer = styled.div`
-    margin: 0 auto;
-    padding: 1em;
-    background: white;
-    border-radius: 10px;
-    width: 100vw;
+    p a{
+        text-decoration: none;
+        color: ${color.PRIMARY};
+    }
 
 `;
-
-const Form = styled.div`
-    width: 70%;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    text-align: center;
-    background: #FFF;
-    
-    label{
-        align-self: flex-start;
-        color: rgba(0,0,0,0.5)
-    }
-
-    input{
-        margin-bottom: 2em;
-        border: none;
-        border-bottom: 1px solid black;
-        background: transparent;
-    }
-
-    textarea{
-        resize: none;
-    }
-
-    @media ${device.mobileS}{
-        width: 80vw;
-    }
-
-    @media ${device.laptop}{
-        width: 50vw;
-    }
-`;
-
-const ConnectButton = styled(Button)`
-    padding: 0.25em 1em;
-    margin-top: 2em;
-    width: 250px;
-    align-self: flex-end;
-    @media ${device.mobileS}{
-        width: 50vw;
-    }
-
-    @media ${device.laptop}{
-        width: 20vw;
-    }
-    
-`;
-
-
-
 
 const Connect = () => (
     <ConnectContainer id="connect">
         <h2>CONNECT</h2>
         <Socials />
 
-        <FormContainer>
-            <Form>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" placeholder="John Doe"/>
-                
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email"placeholder="johndoe@gmail.com"/>
-                
-                <label for="comment">Comment</label>
-                <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Hey Carlos - great stuff!"></textarea>
-
-                <ConnectButton primary type="submit">Message Me</ConnectButton>
-            </Form>
-        </FormContainer>
+        <p>Email me at: <a href="mailto:carlosfegurgur@gmail.com" target="_top">carlosfegurgur@gmail.com</a></p>
 
     </ConnectContainer>
       
