ba52314e2af25849df0d81e64fd5ea13d0b4205f
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-07 17:40:43 -0600

    added logic for card to display different layouts depending on props

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -2,15 +2,30 @@ import React from 'react'
 import Button from '../Buttons'
 import { StyledCard, CardInfo } from './Card.styled'
 
-const Card = ({ title, desc }) => {
+const Card = ({ title, desc, ...props }) => {
     return(
-        <StyledCard>
-            <CardInfo>
-                <h3>{title}</h3>
-                <p>{desc}</p>
-            </CardInfo>
-        </StyledCard>
+        <div>
+            <StyledCard>
+               <CardInfo {...props}>
+                    <h3>{title}</h3>
+                    <p>{desc}</p>
+                    
+                    {props.project ? (
+                        <>
+                            <Button text={props.text} />
+                            <Button text={props.text2} />
+                        </>
+                    ) : (
+                            <Button {...props}/>
+                    )
+                    }
+                    
+                    
+               </CardInfo>
+            </StyledCard>
+        </div>
     )
 }
+
 
 export default Card
