1b33903687927f998f22eb086ffcdea02dd552b7
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-07 17:39:39 -0600

    added comments for alternate button styles

--- src/components/Buttons/Button.styled.js
+++ src/components/Buttons/Button.styled.js
@@ -27,3 +27,26 @@ const StyledButton = styled.button`
 export {
     StyledButton
 }
+
+
+
+// const WorkButton = styled(Button)`
+//     background: transparent;
+//     border-radius: 3px;
+//     border: 2px solid #c8a415;
+//     color: white;
+//     margin: 1em 1em;
+//     padding: 0.25em 1em;
+//     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+//     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+//     ${props => props.primary && css`
+//         background: #c8a415;
+//         color: #white;
+//     `}
+// `;
+
+// const CardButton = styled(Button)`
+//     margin: 0 auto 1em auto;
+//     margin-left: ${props => props.left || 0}em;
+// `;
