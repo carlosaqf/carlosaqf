a892f11a1ad7d551d4211f0a7acd10fa043243b6
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-07 18:02:18 -0600

    refactor menu.js to pull links through a graphql query

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { StyledMenu } from './Menu.styled'
+import Link from '../Links'
+import { StaticQuery, graphql } from 'gatsby'
 
 
-const Menu = ({ open, ...props }) => {
+
+const Menu = ({ open, menuLinks, ...props }) => {
 
     const isHidden = open ? true : false
     const tabIndex = isHidden ? 0 : -1
@@ -13,18 +16,63 @@ const Menu = ({ open, ...props }) => {
             aria-hidden={!isHidden}
             {...props}
         >
-            <a href="/"
-                tabIndex={tabIndex}
-            >About</a>
-            <a href="/"
-                tabIndex={tabIndex}
-            >Work</a>
-            <a href="/"
-                tabIndex={tabIndex}
-            >Contact</a>
+            <StaticQuery
+                query={graphql`
+                    query MenuLinksQuery {
+                        site {
+                            siteMetadata {
+                                title
+                                menuLinks {
+                                    name
+                                    link
+                                }
+                            }
+                        }
+                    }
+                `}
+                render={data => (
+                    <>
+                        <Link
+                            page={data.site.siteMetadata.menuLinks[1].link} 
+                            text={data.site.siteMetadata.menuLinks[1].name} 
+                        />
+                        <Link
+                            page={data.site.siteMetadata.menuLinks[2].link} 
+                            text={data.site.siteMetadata.menuLinks[2].name} 
+                        />
+                        <Link
+                            page={data.site.siteMetadata.menuLinks[3].link} 
+                            text={data.site.siteMetadata.menuLinks[3].name} 
+                        />
+                        <Link
+                            page={data.site.siteMetadata.menuLinks[4].link} 
+                            text={data.site.siteMetadata.menuLinks[4].name} 
+                        />
+                    </>
+                )}
+            />
+      
+            
+
 
         </StyledMenu>
     )
 }
 
 export default Menu
+
+
+
+// <a href="/"
+            //     tabIndex={tabIndex}
+            // >About</a>
+            // <a href="/"
+            //     tabIndex={tabIndex}
+            // >Work</a>
+            // <a href="/"
+            //     tabIndex={tabIndex}
+            // >Contact</a>
+            // <Link
+            //     page={menuLinks[1].link} 
+            //     text={menuLinks[1].name} 
+            // />
