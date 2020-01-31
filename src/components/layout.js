ed74147d8c595a420b5d1ba6a7e13280ff2d95d6
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-31 15:58:14 -0600

    refacted layout file, removing unnecessary graphql query

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,48 +5,27 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import React from 'react'
+import PropTypes from 'prop-types'
+// import { StaticQuery, graphql } from 'gatsby'
 import Navbar from '../components/Navbar/index'
 import Footer from './Footer'
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            menuLinks {
-              name
-              link
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
       <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <Navbar />
         <div
           style={{
             margin: `0 auto`,
             overflow: `hidden`,
-            // padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
+            padding: `0`,
           }}
         >
-          <Navbar menuLinks={data.site.siteMetadata.menuLinks} />
           <main>{children}</main>
-          <Footer />
         </div>
+        <Footer />
       </>
-    )}
-  />
 )
 
 Layout.propTypes = {
