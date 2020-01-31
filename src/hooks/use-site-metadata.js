c75e47047b15c123f108b28ee18a9fd23d48f1c6
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2020-01-31 16:06:04 -0600

    hook to pull graph ql site meta data

---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,22 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+export const useSiteMetadata = () => {
+    const { site } = useStaticQuery(
+        graphql`
+            query SiteMetaData {
+                site {
+                    siteMetadata {
+                        title
+                        menuLinks {
+                            name
+                            link
+                        }
+                        description
+                        author
+                    }
+                }
+            }
+        `
+    )
+    return site.siteMetadata
+}
