1cc7d41e9a459a779c97299b95cf1f8f6a88b67e
carlosaqf <carlosfegurgur@gmail.com>
2020-02-12 20:21:15 -0600

    update index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Hero from "../components/Hero"
-import Bio from "../components/Bio/Bio"
+import React from 'react'
+import Layout from '../components/layout'
+import Hero from '../components/Hero/Hero'
+import Bio from '../components/Bio/Bio'
 import Projects from '../components/Projects/Projects'
-import Connect from "../components/Connect"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import styled from "styled-components"
-import Footer from '../components/Footer'
-
+import Connect from '../components/Connect'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
-  <Layout style={{
-    overflow: `hidden`,
-  }}>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <Hero />
-    <Bio />
-    <Projects />
-    <Connect />
-  </Layout>
+	<Layout style={{
+		overflow: 'hidden',
+	}}>
+		<SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+		<Hero />
+		<Bio />
+		<Projects />
+		<Connect />
+	</Layout>
 )
 
 export default IndexPage
