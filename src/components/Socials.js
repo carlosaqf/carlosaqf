5068329545d2fb2b5e9a90c04085a843e01f8980
carlosaqf <carlosfegurgur@gmail.com>
2020-02-12 20:23:27 -0600

    refactored icons to separate file

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,51 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { color } from '../components/colors'
 import { device } from './devices'
-import  {Github}  from 'styled-icons/boxicons-logos/Github'
-import {Linkedin} from 'styled-icons/boxicons-logos/Linkedin'
-import {Codepen} from 'styled-icons/boxicons-logos/Codepen'
-import {Twitter} from 'styled-icons/boxicons-logos/Twitter'
-import {Instagram} from 'styled-icons/boxicons-logos/Instagram'
-
-const TwitterLogo = styled(Twitter)`
-    height: 3em;
-    width: 3em;
-    margin: 10px 3em;
-    :hover{
-        color: ${color.PRIMARY};
-    }
-    
-`;
-
-const InstagramLogo = styled(Instagram)`
-    height: 3em;
-    width: 3em;
-    margin: 10px 3em;
-    :hover{
-        color: ${color.PRIMARY};
-    }
-`;
-
-const LinkedinLogo = styled(Linkedin)`
-    height: 3em;
-    width: 3em;
-    margin: 10px 3em;
-    :hover{
-        color: ${color.PRIMARY};
-    }
-`;
-
-const GithubLogo = styled(Github)`
-    height: 3em;
-    width: 3em;
-    margin: 10px 3em;
-    font-size: 1em;
-    :hover{
-        color: ${color.PRIMARY};
-    }
-`;
-
+import { TwitterSocial, InstagramSocial, LinkedinSocial, GithubSocial } from './Icons/Icons'
 
 const SocialContainer = styled.div`
     margin: 0 auto;
@@ -53,7 +9,7 @@ const SocialContainer = styled.div`
     justify-content: center;
     padding-top: 1em;
     padding-bottom: 4em;
-`;
+`
 
 const IconContainer = styled.div`
     display: flex;
@@ -69,20 +25,20 @@ const IconContainer = styled.div`
     @media ${device.tablet}{
         flex-direction: row;
     }
-`;
+`
 
 const Socials = () => (
 
-    <SocialContainer>
-        <IconContainer>
-            <a href="https://www.github.com/carlosaqf" target="_blank"><GithubLogo /></a>
-            <a href="https://www.linkedin.com/in/cfegurgur" target="_blank"><LinkedinLogo /></a>
-        </IconContainer>
-        <IconContainer>
-            <a href="https://www.instagram.com/cfegurgur" target="_blank"><InstagramLogo /></a>
-            <a href="https://www.twitter.com/cfegurgur_" target="_blank"><TwitterLogo /></a>
-        </IconContainer>
-    </SocialContainer>
+	<SocialContainer>
+		<IconContainer>
+			<a href="https://www.github.com/carlosaqf" target="_blank" rel="noopener noreferrer"><GithubSocial /></a>
+			<a href="https://www.linkedin.com/in/cfegurgur" target="_blank" rel="noopener noreferrer"><LinkedinSocial /></a>
+		</IconContainer>
+		<IconContainer>
+			<a href="https://www.instagram.com/cfegurgur" target="_blank" rel="noopener noreferrer"><InstagramSocial /></a>
+			<a href="https://www.twitter.com/cfegurgur_" target="_blank" rel="noopener noreferrer"><TwitterSocial /></a>
+		</IconContainer>
+	</SocialContainer>
 
 )
 
