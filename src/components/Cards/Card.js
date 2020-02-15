d8353c32d3aac88c986e629d1cffa3c6063c35a8
carlosaqf <carlosfegurgur@gmail.com>
2020-02-15 15:58:19 -0600

    Update card animation to be blank unless clicked upon, then display information using animation

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -4,44 +4,51 @@ import Button from '../Buttons'
 import { StyledCard, CardInfo, StyledText } from './Card.styled'
 import { useSpring } from 'react-spring'
 
+
 // eslint-disable-next-line react/prop-types
 const Card = ({ title, desc, ...props }) => {
 
-	const [flipped, set] = useState(false)
-	const { opacity } = useSpring({
-		opacity: flipped ? 1 : 0,
-		transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-		config: { mass: 5, tension: 500, friction: 80 }
+	const [show, setShow] = useState(false)
+
+	const contentProps = useSpring({
+		opacity: show ? 1 : 0,
+		marginTop: show ? 0 : -500
 	})
+
+	// const [flipped, set] = useState(false)
+	// const { opacity } = useSpring({
+	// 	opacity: flipped ? 1 : 0,
+	// 	transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+	// 	config: { mass: 5, tension: 500, friction: 80 }
+	// })
 
 
 	return(
 		<div>
-			<StyledCard>
-				<CardInfo {...props}>
-					<h3>{title}</h3>
-					<StyledText
-						onMouseEnter={() => set(state => !state)}
-						onMouseLeave={() => set(state => !state)}
-						style={{
-							opacity: opacity.interpolate(o => 1 - o)
-						}}
-					>
-						{desc}
-					</StyledText>
+			<StyledCard onClick={() => setShow(!show)}>
 
-					{props.text2 ? (
-						<>
-							<Button text={props.text} to={props.to} />
-							<Button text={props.text2} to={props.to} />
-						</>
-					) : (
-						<Button {...props}/>
-					)
-					}
+				{ !show ? (
+					<StyledText></StyledText>
+				) : (
+					<CardInfo {...props}>
+						<h3>{title}</h3>
+						<StyledText style={contentProps}>
+							{desc}
+						</StyledText>
+						{props.text2 ? (
+							<>
+								<Button text={props.text} to={props.to} />
+								<Button text={props.text2} to={props.to} />
+							</>
+						) : (
+							<Button {...props}/>
+						)
+						}
+					</CardInfo>
+				)}
 
 
-				</CardInfo>
+
 			</StyledCard>
 			{/* <StyledCard className='c front' style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
 				<CardInfo {...props}>
