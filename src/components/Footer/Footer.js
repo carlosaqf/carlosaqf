a45b2c23db3b1a6454100040bc2870c775eb00d4
carlosaqf <carlosfegurgur@gmail.com>
2020-02-15 15:50:55 -0600

    Reorder icons to match connect portion

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,8 +12,8 @@ const Footer = () => (
 			<a href="/">Carlos Fegurgur &copy; {new Date().getFullYear()}</a>
 			<a href="https://www.github.com/carlosaqf" target="_blank" rel="noopener noreferrer"><GithubFooter /></a>
 			<a href="https://www.linkedin.com/in/cfegurgur" target="_blank" rel="noopener noreferrer"><LinkedinFooter /></a>
-			<a href="https://www.twitter.com/cfegurgur_" target="_blank" rel="noopener noreferrer"><TwitterFooter /></a>
 			<a href="https://www.instagram.com/cfegurgur" target="_blank" rel="noopener noreferrer"><InstagramFooter /></a>
+			<a href="https://www.twitter.com/cfegurgur_" target="_blank" rel="noopener noreferrer"><TwitterFooter /></a>
 		</StyledFooterLinks>
 	</StyledFooter>
 )
