0afbecc949a4ffc925a182fb707b435a4359f277
carlosaqf <carlosfegurgur@gmail.com>
2020-02-20 11:36:50 -0600

    Added prop type validation to Button

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { StyledButton } from './Button.styled'
+import PropTypes from 'prop-types'
 
 const Button = ({ text, to, ...props }) => {
-    return(
-        <a href={to}>
-            <StyledButton {...props}>{text}</StyledButton>
-        </a>
-    )
+	return(
+		<a href={to}>
+			<StyledButton {...props}>{text}</StyledButton>
+		</a>
+	)
+}
+
+Button.propTypes = {
+	text: PropTypes.string,
+	to: PropTypes.string
 }
 
 export default Button
