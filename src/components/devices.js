e6f98eff4f9e5cf0171480d01a6662ce982f5b3a
carlosaqf <carlosfegurgur@gmail.com>
2020-02-20 11:41:12 -0600

    reuploaded devices file

--- src/components/devices.js
+++ src/components/devices.js
@@ -1,32 +1,33 @@
+/* eslint-disable linebreak-style */
 const size = {
-    mobileS: '320px',
-    mobileM: '375px',
-    mobileL: '425px',
-    tablet: '768px',
-    laptop: '1024px',
-    laptopL: '1440px',
-    desktop: '2560px',
+	mobileS: '320px',
+	mobileM: '375px',
+	mobileL: '425px',
+	tablet: '768px',
+	laptop: '1024px',
+	laptopL: '1440px',
+	desktop: '2560px',
 }
 
 export const device = {
-    mobileS: `(min-width: ${size.mobileS})`,
-    // Anything bigger than 320px
+	mobileS: `(min-width: ${size.mobileS})`,
+	// Anything bigger than 320px
 
-    mobileM: `(min-width: ${size.mobileM})`,
-    // Anything bigger than 375px
+	mobileM: `(min-width: ${size.mobileM})`,
+	// Anything bigger than 375px
 
-    mobileL: `(min-width: ${size.mobileL})`,
-    // Anything bigger than 425px
+	mobileL: `(min-width: ${size.mobileL})`,
+	// Anything bigger than 425px
 
-    tablet: `(min-width: ${size.tablet})`,
-    // Anything bigger than 768px
+	tablet: `(min-width: ${size.tablet})`,
+	// Anything bigger than 768px
 
-    laptop: `(min-width: ${size.laptop})`,
-    // Anything bigger than 1024px
+	laptop: `(min-width: ${size.laptop})`,
+	// Anything bigger than 1024px
 
-    laptopL: `(min-width: ${size.laptopL})`,
-    // Anything bigger than 1440px
+	laptopL: `(min-width: ${size.laptopL})`,
+	// Anything bigger than 1440px
 
-    desktop: `(min-width: ${size.desktop})`,
-    // Anything bigger than 2560px
+	desktop: `(min-width: ${size.desktop})`,
+	// Anything bigger than 2560px
 }
