c517316629db3f34f5f6fc52cb9ef63e90379ba3
carlosaqf <carlosfegurgur@gmail.com>
2020-02-23 10:23:50 -0600

    update gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,79 +1,80 @@
+/* eslint-disable no-undef */
 module.exports = {
-  siteMetadata: {
-    title: `Carlos Fegurgur | Web Developer | Chicago, IL`,
-    menuLinks:[
-      {
-        name:'home',
-        link:'/'
-      },
-      {
-        name:'about',
-        link:'/about'
-      },
-      {
-        name:'work',
-        link:'/work'
-      },
-      {
-        name:'blog',
-        link:'/blog'
-      },
-      {
-        name:'contact',
-        link:'/#connect'
-      }
-    ],
-    description: `Personal website of Carlos Fegurgur. A showcase of projects, thoughts and ideas. As well as an insight in to the man behind the screen.`,
-    author: `@carlosaqf`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
-    `gatsby-plugin-react-helmet`, 
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /\.inline\.svg$/
-        }
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-transformer-remark`,
-    'gatsby-plugin-offline',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages",
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-  ],
+	siteMetadata: {
+		title: 'Carlos Fegurgur | Web Developer | Chicago, IL',
+		menuLinks:[
+			{
+				name:'home',
+				link:'/'
+			},
+			{
+				name:'about',
+				link:'/about'
+			},
+			{
+				name:'work',
+				link:'/work'
+			},
+			{
+				name:'blog',
+				link:'/blog'
+			},
+			{
+				name:'contact',
+				link:'/#connect'
+			}
+		],
+		description: 'Personal website of Carlos Fegurgur. A showcase of projects, thoughts and ideas. As well as an insight in to the man behind the screen.',
+		author: '@carlosaqf',
+	},
+	plugins: [
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'src',
+				path: `${__dirname}/src/`,
+			},
+		},
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-styled-components',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images`,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-react-svg',
+			options: {
+				rule: {
+					include: /\.inline\.svg$/
+				}
+			}
+		},
+		'gatsby-transformer-sharp',
+		'gatsby-plugin-sharp',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				display: 'minimal-ui',
+				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+			},
+		},
+		'gatsby-transformer-remark',
+		'gatsby-plugin-offline',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: 'pages',
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// 'gatsby-plugin-offline',
+	],
 }
