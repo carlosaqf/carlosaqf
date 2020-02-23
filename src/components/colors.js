9a386a3e04e30d8964abab917ee6301d87aac3db
carlosaqf <carlosfegurgur@gmail.com>
2020-02-23 10:21:57 -0600

    updated colors file

--- src/components/colors.js
+++ src/components/colors.js
@@ -1,31 +1,20 @@
 export const color = {
-    PRIMARY: '#c8a415',
-    SECONDARY: '#ffd54f',
-    
-    DARK: 'rgba(200,164,21,1)',
-    LIGHT: 'rgba(255, 213, 79, 1)',
-    GRAY: 'rgba(97, 97, 97, 1)',
-    WHITE: 'rgba(253, 255, 252, 1)',
-    BLUE: 'rgba(35, 87, 137, 1)',
+	PRIMARY: '#c8a415',
+	SECONDARY: '#ffd54f',
 
-    SHADOW: `0 2.8px 2.2px rgba(0, 0, 0, 0.02),
+	DARK: 'rgba(200,164,21,1)',
+	LIGHT: 'rgba(255, 213, 79, 1)',
+	GRAY: 'rgba(97, 97, 97, 1)',
+	WHITE: 'rgba(253, 255, 252, 1)',
+	BLUE: 'rgba(35, 87, 137, 1)',
+
+	SHADOW: `0 2.8px 2.2px rgba(0, 0, 0, 0.02),
     0 6.7px 5.3px rgba(0, 0, 0, 0.028),
     0 12.5px 10px rgba(0, 0, 0, 0.035),
     0 22.3px 17.9px rgba(0, 0, 0, 0.042),
     0 41.8px 33.4px rgba(0, 0, 0, 0.05),
     0 100px 80px rgba(0, 0, 0, 0.07)
   `,
-    SHADOWTWO: '0 3px 4px 0 rgba(0,0,0,0.5)',
+	SHADOWTWO: '0 3px 4px 0 rgba(0,0,0,0.5)',
 
 }
-
-
-/*
-yellow #F4D06F
-black #0D1F22
-grey blue #232C33
-blue #0075A2
-white #ECEBF3
-
-
-*/
