357f0347558a8cc78dc8a6df19fa4bfe8dbda1ca
carlosaqf <carlosfegurgur@gmail.com>
2020-07-02 19:51:54 -0500

    Grammar fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,13 +3,12 @@ import React from 'react'
 import Button from '../Buttons'
 import { HeroContainer, ButtonContainer } from './Hero.styled'
 
-
 const Hero = () => {
 
 	return(
 		<HeroContainer>
 
-			<h2>Hafa Adai</h2>
+			<h2>Hafa Adai, I'm</h2>
 			<h1>Carlos Fegurgur</h1>
 			<p>Front End Web Developer | Chicago, IL</p>
 
