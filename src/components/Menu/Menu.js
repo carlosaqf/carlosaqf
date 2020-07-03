d3ca3b4bb69808fd3af4e162c208b321ce9acae2
carlosaqf <carlosfegurgur@gmail.com>
2020-07-02 19:52:43 -0500

    Update menu behavior. Menu only appears when burger button is clicked.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,36 +1,37 @@
 import React from 'react'
-import { StyledMenu } from './Menu.styled'
+import { StyledMenu, MenuLinks } from './Menu.styled'
 import Link from '../Links'
 import { useSiteMetadata } from '../../hooks/use-site-metadata'
 import PropTypes from 'prop-types'
 
 const Menu = ({ open, ...props }) => {
 
-	const isHidden = open ? true : false
 	const { menuLinks } = useSiteMetadata()
 
 	return (
 		<StyledMenu
 			open={open}
-			aria-hidden={!isHidden}
 			{...props}
 		>
-			<Link
-				page={menuLinks[1].link}
-				text={menuLinks[1].name}
-			/>
-			<Link
-				page={menuLinks[2].link}
-				text={menuLinks[2].name}
-			/>
-			<Link
-				page={menuLinks[3].link}
-				text={menuLinks[3].name}
-			/>
-			<Link
-				page={menuLinks[4].link}
-				text={menuLinks[4].name}
-			/>
+			<MenuLinks>
+				<Link
+					page={menuLinks[1].link}
+					text={menuLinks[1].name}
+					/>
+				<Link
+					page={menuLinks[2].link}
+					text={menuLinks[2].name}
+					/>
+				<Link
+					page={menuLinks[3].link}
+					text={menuLinks[3].name}
+					/>
+				<Link
+					page={menuLinks[4].link}
+					text={menuLinks[4].name}
+				/>
+			</MenuLinks>
+
 		</StyledMenu>
 	)
 }
--- src/components/Menu/Menu.styled.js
+++ src/components/Menu/Menu.styled.js
@@ -1,50 +1,50 @@
 import styled from 'styled-components'
 import { color } from '../colors'
 
-export const StyledMenu = styled.nav`
-    display: flex;
+const StyledMenu = styled.div`
+
+    display: ${({ open }) =>
+        open
+            ? 'flex'
+            : 'none'
+    };
     flex-direction: column;
     justify-content: center;
-    background: ${color.SECONDARY};
-    transform: ${({ open }) =>
-		open
-			? 'translateX(0)'
-			: 'translateX(-100%)'
-};
-    height: 100vh;
+    background: ${color.WHITE};
+
+    height: 30vh;
+    width: 100vw;
     text-align: left;
     padding: 2rem;
     position: absolute;
     top: 0;
-    left: 0;
     transition: transform 0.3s ease-in-out;
     list-style-type: none;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: 300;
 
-    @media (max-width: '768px'){
-        width: 100%;
-    }
-
-    a {
-        font-size: 2rem;
-        text-transform: uppercase;
-        /* padding: 2rem 0; */
-        font-weight: 500;
-        letter-spacing: 0.5rem;
-        color: ${color.DARK};
-        text-decoration: none;
-        transition: color 0.3s linear;
-        
-        
-        @media (max-width: '768px'){
-            font-size: 1.5rem;
-            text-align: center;
-        }
-
-        &:hover{
-            color: ${color.LIGHT}
-        }
-    }
 `
+
+const MenuLinks = styled.ul`
+    margin: 0 auto;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family: Helvetica;
+    font-size: 1em;
+    
+    a{
+        text-decoration: none;
+        color: ${color.MAIN_BRAND};
+        padding: 0.6em;
+        font-weight: 300;
+        font-size: 1em;
+        transition: all 300ms linear 0s;
+    }
+
+`
+
+export {
+    StyledMenu,
+    MenuLinks
+}
