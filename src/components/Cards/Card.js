11d35421351c77f82c4ab667877f8236db31f318
carlosaqf <carlosfegurgur@gmail.com>
2020-07-05 09:01:00 -0500

    simplified card layout. removed animation

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,19 +1,50 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '../Buttons'
-import { StyledCard, CardInfo, StyledText } from './Card.styled'
-import { useSpring } from 'react-spring'
+import {
+	StyledCard,
+	CardImg,
+	CardInfo,
+	CardInfoHeader,
+	CardInfoDescription
+} from './Card.styled'
 
-
-// eslint-disable-next-line react/prop-types
 const Card = ({ title, desc, ...props }) => {
 
-	const [show, setShow] = useState(false)
+	return (
 
-	const contentProps = useSpring({
-		opacity: show ? 1 : 0,
-		marginTop: show ? 0 : -500
-	})
+		<StyledCard>
+
+			<CardImg src={'https://source.unsplash.com/random/400x400'} />
+
+			<CardInfo>
+
+				<CardInfoHeader>{title}</CardInfoHeader>
+
+				<CardInfoDescription>{desc}</CardInfoDescription>
+
+				<Button primary {...props}/>
+
+			</CardInfo>
+
+		</StyledCard>
+
+	)
+
+
+
+
+
+
+
+
+
+	// const [show, setShow] = useState(false)
+
+	// const contentProps = useSpring({
+	// 	opacity: show ? 1 : 0,
+	// 	marginTop: show ? 0 : -500
+	// })
 
 	// const [flipped, set] = useState(false)
 	// const { opacity } = useSpring({
@@ -23,54 +54,72 @@ const Card = ({ title, desc, ...props }) => {
 	// })
 
 
-	return(
-		<div>
-			<StyledCard onClick={() => setShow(!show)}>
+	// return(
+	// <div>
+	// <StyledCard
+	// 	// onClick={() => setShow(!show)}
+	// >
 
-				{ !show ? (
-					<StyledText></StyledText>
-				) : (
-					<CardInfo {...props}>
-						<h3>{title}</h3>
-						<StyledText style={contentProps}>
-							{desc}
-						</StyledText>
-						{props.text2 ? (
-							<>
-								<Button text={props.text} to={props.to} />
-								<Button text={props.text2} to={props.to} />
-							</>
-						) : (
-							<Button {...props}/>
-						)
-						}
-					</CardInfo>
-				)}
+	// 	{/* { !show ? (
+	// 		<StyledText></StyledText>
+	// 	) : (
+	// 		<CardInfo {...props}>
+	// 			<h3>{title}</h3>
+	// 			<StyledText style={contentProps}>
+	// 				{desc}
+	// 			</StyledText>
+	// 			{props.text2 ? (
+	// 				<>
+	// 					<Button text={props.text} to={props.to} />
+	// 					<Button text={props.text2} to={props.to} />
+	// 				</>
+	// 			) : (
+	// 				<Button {...props}/>
+	// 			)
+	// 			}
+	// 		</CardInfo>
+	// 	)} */}
+
+	// 	<CardInfo {...props}>
+	// 		<h3>{title}</h3>
+	// 		<StyledText>
+	// 			{desc}
+	// 		</StyledText>
+	// 		{props.text2 ? (
+	// 			<>
+	// 				<Button text={props.text} to={props.to} />
+	// 				<Button text={props.text2} to={props.to} />
+	// 			</>
+	// 		) : (
+	// 			<Button {...props}/>
+	// 		)
+	// 		}
+	// 	</CardInfo>
 
 
-
-			</StyledCard>
-			{/* <StyledCard className='c front' style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
-				<CardInfo {...props}>
-					<h3>{title}</h3>
-					<p>{desc}</p>
-
-					{props.text2 ? (
-						<>
-							<Button text={props.text} to={props.to} />
-							<Button text={props.text2} to={props.to} />
-						</>
-					) : (
-						<Button {...props}/>
-					)
-					}
-
-
-				</CardInfo>
-			</StyledCard> */}
-		</div>
-	)
+	// </StyledCard>
+	// )
 }
+
+{/* <StyledCard className='c front' style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
+		<CardInfo {...props}>
+			<h3>{title}</h3>
+			<p>{desc}</p>
+
+			{props.text2 ? (
+				<>
+					<Button text={props.text} to={props.to} />
+					<Button text={props.text2} to={props.to} />
+				</>
+			) : (
+				<Button {...props}/>
+			)
+			}
+
+
+		</CardInfo>
+	</StyledCard>
+	</div> */}
 
 
 export default Card
