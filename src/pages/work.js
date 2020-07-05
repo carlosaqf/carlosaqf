2b3a951c5d5a8a64aa275d1d8d935f969ad6ffb4
carlosaqf <carlosfegurgur@gmail.com>
2020-07-05 09:07:08 -0500

    update design of Work page to reflect changes made in Card component. Layout simplified to be single column regardless of screen size

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,54 +1,30 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Button from '../components/Buttons'
-import { device } from '../components/devices'
 import Card from '../components/Cards'
 
 const WorkContainer = styled.div`
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background: #fafafa;
-  padding-bottom: 10px;
-  margin: 0 auto;
-  min-height: 96.5vh;
+  padding-bottom: 2rem;
+
   h2{
       font-size: 2em;
       letter-spacing: 0.8em;
       font-weight: 200;
       padding-top: 1em;
-      text-align: center;
-      text-transform: uppercase;
   }
 `
 
 const CardsContainer = styled.div`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    max-width: 90vw;
-    // justify-content: center;
-
-    @media ${device.mobileS}{
-        grid-template-columns: 1fr;
-    }
-
-    @media ${device.tablet}{
-      grid-template-columns: 1fr 1fr;
-    }
-
-    @media ${device.laptop}{
-      grid-template-columns: repeat(3, 1fr);
-    }
+    grid-template-columns: 1fr;
+    max-width: 66em;
+    justify-content: center;
 `
-
-
-
-
-
-
 
 const Work = () => (
 
@@ -91,26 +67,9 @@ const Work = () => (
 					text='Visit Demo'
 					to='https://www.github.com/carlosaqf/notes'
 				/>
-				<Card
-					title='Project 6'
-					desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, magnam.'
-					text='Visit Demo'
-					to='https://www.github.com/carlosaqf/notes'
-				/>
-				<Card
-					title='Project 7'
-					desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, magnam.'
-					text='Visit Demo'
-					to='https://www.github.com/carlosaqf/notes'
-				/>
-				<Card
-					title='Project 8'
-					desc='Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, magnam.'
-					text='Visit Demo'
-					to='https://www.github.com/carlosaqf/notes'
-				/>
 
 			</CardsContainer>
+
 		</WorkContainer>
 
 	</Layout>
