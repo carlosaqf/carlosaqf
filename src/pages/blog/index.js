96db8918b86e8e905bc077071dae53f23baea275
carlosaqf <carlosfegurgur@gmail.com>
2020-07-14 07:53:41 -0500

    changes made to separate blog and project page creation functionality

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,90 +1,61 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
-import { device } from '../../components/devices'
-
-const BlogWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background: #fafafa;
-  padding: 0 1em 0.8em 1em;
-  margin: 0 auto;
-  h2{
-      font-size: 2em;
-      letter-spacing: 0.8em;
-      font-weight: 200;
-      padding-top: 1em;
-      text-align: center;
-      text-transform: uppercase;
-  }
-`
-
-const BlogPost = styled.div`
-  margin: 1em 2em;
-  text-align: center;
-  background: #c7c7c7;
-  border-radius: 0.5em;
-  width: 60vw;
-
-  @media ${device.mobileS}{
-    width: 90vw;
-  }
-
-  @media ${device.laptop}{
-    width: 45vw;
-  }
-`
-
-const BlogImg = styled.img`
-  width: 100vw;
-  height: 40vh;
-  margin: 0 auto;
-  padding: 0;
-  border-radius: 0.5em 0.5em 0 0;
-`
+import {
+	BlogWrapper,
+	BlogPost,
+	BlogImg
+} from './Posts.styled'
 
 
-const Blog = ({ data }) => {
-	return (
-		<Layout>
-			<SEO title="Blog" />
-			<BlogWrapper>
-				<h2>Blog</h2>
-				<p>Welcome to the Blog Page. Here I will explain who I am and what
+const Blog = ({ data }) => (
+	<Layout>
+		<SEO title="Blog" />
+		<BlogWrapper>
+			<h2>Blog</h2>
+			<p>Welcome to the Blog Page. Here I will explain who I am and what
           it is I hope to accomplish with this blog. Get to know me!
-				</p>
-				<h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-				{data.allMarkdownRemark.edges.map(({ node }) => (
+			</p>
+			<h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+			{data.allMarkdownRemark.edges.map(({ node }) => (
+				(node.frontmatter.type === 'blog') ? (
 					<BlogPost key={node.id}>
 						<Link
 							to={node.fields.slug}
 							style={{
 								textDecoration: 'none',
 								color: 'inherit',
-							}} >
-							<BlogImg src={node.frontmatter.image}></BlogImg>
+							}}>
+							<BlogImg src={node.frontmatter.image} />
 							<h3 style={{
 								textAlign: 'left',
 								paddingTop: '0.5em',
 								paddingLeft: '0.5em',
-							}}>{node.frontmatter.title} - {node.frontmatter.date}</h3>
+							}}>
+								{node.frontmatter.title} - {node.frontmatter.date}
+							</h3>
 							<p>{node.excerpt}</p>
 						</Link>
 					</BlogPost>
-				))}
-			</BlogWrapper>
-		</Layout>
-	)
-}
+				) : (
+					null
+				)
+			))}
+		</BlogWrapper>
+	</Layout>
+)
+
 
 export default Blog
 
 export const query = graphql`
   query{
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+		filter: { frontmatter: { type: {eq: "blog"} } },
+		sort: { fields: [frontmatter___date], order: DESC }
+	){
       totalCount
       edges {
         node {
@@ -94,6 +65,7 @@ export const query = graphql`
             date(formatString: "DD MMMM, YYYY")
             image
             author
+            type
           }
           fields {
             slug
