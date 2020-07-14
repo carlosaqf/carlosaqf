3e5d51b2fc6dea711449ec48e5cf04c926c76065
carlosaqf <carlosfegurgur@gmail.com>
2020-07-14 07:51:49 -0500

    Made card image variable passed in as a prop

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -15,7 +15,7 @@ const Card = ({ title, desc, ...props }) => {
 
 		<StyledCard>
 
-			<CardImg src={'https://source.unsplash.com/random/400x400'} />
+			<CardImg src={props.src} />
 
 			<CardInfo>
 
@@ -30,76 +30,76 @@ const Card = ({ title, desc, ...props }) => {
 		</StyledCard>
 
 	)
-
-
-
-
-
-
-
-
-
-	// const [show, setShow] = useState(false)
-
-	// const contentProps = useSpring({
-	// 	opacity: show ? 1 : 0,
-	// 	marginTop: show ? 0 : -500
-	// })
-
-	// const [flipped, set] = useState(false)
-	// const { opacity } = useSpring({
-	// 	opacity: flipped ? 1 : 0,
-	// 	transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-	// 	config: { mass: 5, tension: 500, friction: 80 }
-	// })
-
-
-	// return(
-	// <div>
-	// <StyledCard
-	// 	// onClick={() => setShow(!show)}
-	// >
-
-	// 	{/* { !show ? (
-	// 		<StyledText></StyledText>
-	// 	) : (
-	// 		<CardInfo {...props}>
-	// 			<h3>{title}</h3>
-	// 			<StyledText style={contentProps}>
-	// 				{desc}
-	// 			</StyledText>
-	// 			{props.text2 ? (
-	// 				<>
-	// 					<Button text={props.text} to={props.to} />
-	// 					<Button text={props.text2} to={props.to} />
-	// 				</>
-	// 			) : (
-	// 				<Button {...props}/>
-	// 			)
-	// 			}
-	// 		</CardInfo>
-	// 	)} */}
-
-	// 	<CardInfo {...props}>
-	// 		<h3>{title}</h3>
-	// 		<StyledText>
-	// 			{desc}
-	// 		</StyledText>
-	// 		{props.text2 ? (
-	// 			<>
-	// 				<Button text={props.text} to={props.to} />
-	// 				<Button text={props.text2} to={props.to} />
-	// 			</>
-	// 		) : (
-	// 			<Button {...props}/>
-	// 		)
-	// 		}
-	// 	</CardInfo>
-
-
-	// </StyledCard>
-	// )
 }
+
+
+
+
+
+
+
+
+// const [show, setShow] = useState(false)
+
+// const contentProps = useSpring({
+// 	opacity: show ? 1 : 0,
+// 	marginTop: show ? 0 : -500
+// })
+
+// const [flipped, set] = useState(false)
+// const { opacity } = useSpring({
+// 	opacity: flipped ? 1 : 0,
+// 	transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+// 	config: { mass: 5, tension: 500, friction: 80 }
+// })
+
+
+// return(
+// <div>
+// <StyledCard
+// 	// onClick={() => setShow(!show)}
+// >
+
+// 	{/* { !show ? (
+// 		<StyledText></StyledText>
+// 	) : (
+// 		<CardInfo {...props}>
+// 			<h3>{title}</h3>
+// 			<StyledText style={contentProps}>
+// 				{desc}
+// 			</StyledText>
+// 			{props.text2 ? (
+// 				<>
+// 					<Button text={props.text} to={props.to} />
+// 					<Button text={props.text2} to={props.to} />
+// 				</>
+// 			) : (
+// 				<Button {...props}/>
+// 			)
+// 			}
+// 		</CardInfo>
+// 	)} */}
+
+// 	<CardInfo {...props}>
+// 		<h3>{title}</h3>
+// 		<StyledText>
+// 			{desc}
+// 		</StyledText>
+// 		{props.text2 ? (
+// 			<>
+// 				<Button text={props.text} to={props.to} />
+// 				<Button text={props.text2} to={props.to} />
+// 			</>
+// 		) : (
+// 			<Button {...props}/>
+// 		)
+// 		}
+// 	</CardInfo>
+
+
+// </StyledCard>
+// )
+
 
 {/* <StyledCard className='c front' style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
 		<CardInfo {...props}>
