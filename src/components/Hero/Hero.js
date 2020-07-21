4b83b8e7626b1e3b0a92521362a4d46cc154004e
carlosaqf <carlosfegurgur@gmail.com>
2020-07-21 18:39:14 -0500

    Edited information found in Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,31 +1,35 @@
 /* eslint-disable linebreak-style */
 import React from 'react'
 import Button from '../Buttons'
-import { HeroContainer, ButtonContainer } from './Hero.styled'
+import { HeroContainer, HeroText, ButtonContainer, StyledHero } from './Hero.styled'
 
 const Hero = () => {
 
 	return(
 		<HeroContainer>
+			<StyledHero>
+				<HeroText>
+					<h3>Hafa Adai, I&apos;m</h3>
+					<h1>Carlos Fegurgur</h1>
+					<p>I am a Front End Web Developer originally from the island of Guam, now living in Chicago.
+				I am passionate about solving problems and constantly looking for new challenges or ways to
+				learn and improve! I&apos;d love to learn about how I can solve your problems, especially if the
+				solution involves a web application of some kind. I&apos;m only an email away!
+					</p>
+					<ButtonContainer>
+						<Button
+							text='Learn More'
+							to='/about'
+							primary
+						/>
+						<Button
+							text='View Work'
+							to='#projects'
+						/>
 
-			<h2>Hafa Adai, I&apos;m</h2>
-			<h1>Carlos Fegurgur</h1>
-			<p>Front End Web Developer | Chicago, IL</p>
-
-
-			<ButtonContainer>
-				<Button
-					text='Learn More'
-					to='#bio'
-					primary
-				/>
-
-				<Button
-					text='Reach Out'
-					to='#connect'
-				/>
-
-			</ButtonContainer>
+					</ButtonContainer>
+				</HeroText>
+			</StyledHero>
 
 		</HeroContainer>
 	)
