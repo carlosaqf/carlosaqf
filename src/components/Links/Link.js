dc2df5fac0a2e1c4371277359bd8652e36fbbe2b
carlosaqf <carlosfegurgur@gmail.com>
2020-07-25 10:16:55 -0500

    updated Link to include activeStyle prop

--- src/components/Links/Link.js
+++ src/components/Links/Link.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { StyledLink } from './Link.styled'
 import PropTypes from 'prop-types'
 
-const Link = ({ page, text }) => {
+const Link = ({ page, text, activeStyle }) => {
 
 	return(
 		<li>
-			<StyledLink to={page}>{text}</StyledLink>
+			<StyledLink to={page} activeStyle={{ color: activeStyle }} >{text}</StyledLink>
 		</li>
 	)
 }
 
 Link.propTypes = {
 	page: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
+	activeStyle: PropTypes.string
 }
 
 export default Link
