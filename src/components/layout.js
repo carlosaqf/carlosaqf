0fb2736958aa22639dbad8a724b01a39fd24dd19
carlosaqf <carlosfegurgur@gmail.com>
2020-07-25 10:13:28 -0500

    changed import filepaths

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import Navbar from '../components/Navbar/index'
-import Footer from '../components/Footer/index'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
