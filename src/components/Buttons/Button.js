2646a661a07379716a29792a923a3082007e6e63
carlosaqf <carlosfegurgur@gmail.com>
2020-07-31 18:25:50 -0500

    Button component updates

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types'
 const Button = ({ text, to, ...props }) => {
 	return(
 		<a href={to} download={props.download}>
-			<StyledButton {...props}>{text}</StyledButton>
+			<StyledButton {...props}>
+				{text}
+			</StyledButton>
 		</a>
 	)
 }
--- src/components/Buttons/Button.styled.js
+++ src/components/Buttons/Button.styled.js
@@ -2,17 +2,19 @@ import styled, { css } from 'styled-components'
 import { color } from '../colors'
 
 const StyledButton = styled.button`
-    background: transparent;
-    border-radius: 1em;
-    border: 0.1em solid ${color.MAIN_BRAND};
-    color: white;
-    margin: 0 1em;
-    padding: 0.25em 1em;
-    font-family: Arial, Helvetica, sans-serif;
+    background: ${color.MAIN_BRAND};
+    border-radius: 0.5em;
+    border: none;
+    padding: 0.5em 1.25em;
     box-shadow: ${color.SHADOWTWO};
-    
+    font-family: 'Roboto', sans-serif;
+    a{
+        text-decoration: none;
+        color: ${color.SHADE_DARK};  
+    }
     &:hover{
-        color: ${color.MAIN_BRAND};
+        color: ${color.SHADE_LIGHT};
+        transition: all 200ms linear 0s;
     }
 
     &:active{
@@ -26,12 +28,24 @@ const StyledButton = styled.button`
 
 
     /* DEFINE BUTTON STYLES THROUGH PROPS */
-    ${props => props.primary && css`
-        background: ${color.MAIN_BRAND};
-        color: ${color.SHADE_DARK};
-        margin: 0;
+    ${props => props.secondary && css`
+        background: transparent;
+        box-shadow: none;
+        color: ${color.SHADE_LIGHT};
         & :hover{
-            color: ${color.SHADE_LIGHT};
+            color: ${color.MAIN_BRAND};
+        }
+    `}
+    
+    ${props => props.inverse && css`
+        background: transparent;
+        box-shadow: none;
+        border: 1px solid ${color.SHADE_DARK};
+        color: ${color.SHADE_DARK};
+
+        & :hover{
+            color: ${color.MAIN_BRAND};
+            border: 1px solid ${color.MAIN_BRAND};
         }
     `}
 
